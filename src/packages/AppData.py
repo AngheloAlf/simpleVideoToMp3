443b04e57d6660212956571e668bff505117fba3
Anghelo <angheloalf95@gmail.com>
2017-10-13 11:19:08 -0300

    Transformar un puro video

--- src/packages/AppData.py
+++ src/packages/AppData.py
@@ -27,6 +27,8 @@ class AppData:
         self.gui.addTab("Descargar", GuiGenerator.downloadTab)
 
         self.gui.addTab("Transformar", GuiGenerator.transformTab)
+        self.gui.buttons["transformSimple"][0]["command"] = self.transformVideo
+        self.gui.buttons["transformSimple"][0]["state"] = "normal"
         self.gui.buttons["transformFolder"][0]["command"] = self.transformFolder
         self.gui.buttons["transformFolder"][0]["state"] = "normal"
 
@@ -53,6 +55,8 @@ class AppData:
         folderOut = GuiManager.selectFolder(u"Seleccione su carpeta de destino")
         if not folderOut:
             return
+        if "." not in folderOut:
+            folderOut += ".mp3"
 
         self.gui.disableAll()
         errors = Transformer.FolderTransformer(folderSrc, folderOut).transform()
@@ -62,44 +66,20 @@ class AppData:
         else:
             GuiManager.popupWarning(u"Problemas.", str(errors) + u" archivos no han podido ser transformados.")
 
+    def transformVideo(self):
+        videoSrc = GuiManager.openFile(u"Seleccione su video.", (("MP4", "*.mp4"), ("Todo", "*.*")))
+        if not videoSrc:
+            return
+        audioOut = GuiManager.saveFile(u"Indique el nombre del archivo final.", (("MP3", "*.mp3"), ("Todo", "*.*")))
+        if not audioOut:
+            return
 
-#
-# folder1 = filedialog.askdirectory(title="Select videos folder")
-# if folder1 == "":
-#     exit(0)
-# folder2 = filedialog.askdirectory(title="Select output folder")
-# if folder2 == "":
-#     exit(0)
-#
-# indice = 0
+        self.gui.disableAll()
+        success = Transformer.Transformer(videoSrc, audioOut).transform()
+        self.gui.enableAll()
+        if success:
+            GuiManager.popupInfo(u"Exito.", u"Se ha transformado su video exitosamente..")
+        else:
+            GuiManager.popupWarning(u"Problemas.", u"Su archivo no han podido ser transformado.")
+
 # total = len([name for name in os.listdir(folder1) if os.path.isfile(os.path.join(folder1, name))])
-# print(total)
-#
-# subResultFolder = os.path.split(folder1)[-1]
-# audioFolder = os.path.join(folder2, subResultFolder)
-# if not os.path.isdir(audioFolder):
-#     os.mkdir(audioFolder)
-#
-# for videoName in os.listdir(folder1):
-#     folderAndVideo = os.path.join(folder1, videoName)
-#     if os.path.isfile(folderAndVideo):
-#         indice += 1
-#
-#         subName = videoName.split(".")
-#         subName[-1] = "mp3"
-#         audioName = ".".join(subName)
-#
-#         folderAndAudio = os.path.join(audioFolder, audioName)
-#
-#         if os.path.isfile(folderAndAudio):
-#             continue
-#
-#         print("\n")
-#         print(videoName + " -> " + audioName)
-#         print("\t" + folderAndVideo + " -> " + folderAndAudio)
-#         print("\n")
-#
-#         clip = mp.VideoFileClip(folderAndVideo)
-#         clip.audio.write_audiofile(folderAndAudio)
-#
-#         print("\t" + str(indice) + "/" + str(total) + " ~ " + str(100 * indice / total) + "%")
